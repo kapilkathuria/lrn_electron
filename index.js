dc86bb9659f1e6919ca9fe72afb12fa73851dc15
kapil kathuria <kapil.kathuria@gmail.com>
2020-10-25 14:56:21 +0530

    section 2 - crated desktop app and added option to browse a file

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+'use scrict';
+
 const electron = require('electron');
+const path = require('path') 
+require('dotenv').config();
+
+const env = process.env.NODE_ENV || 'development'; 
+
+// If development environment 
+if (env === 'development') { 
+	require('electron-reload')(__dirname, { 
+		electron: path.join(__dirname, 'node_modules', '.bin', 'electron'), 
+		hardResetMethod: 'exit'
+	}); 
+} 
 
 
 // electron starts --> app process is  created --> app ready events --> app closes down
-// 
-const { app } = electron; 
+// BrowserWindow: will start new Browser Window in electron
+const { app, BrowserWindow } = electron; 
 
 app.on( 'ready',() => {
     // do something
-    console.log('app is now ready');
+    const mainWindow = new BrowserWindow ({});
+    console.log('App is now ready');
+    // we can also load website such as follows
+    // mainWindow.loadURL('https://www.google.com/');
+    // mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.loadFile('index.html');
 });
--- section2.js
+++ section2.js
@@ -3,3 +3,14 @@
 
 // electron lifecycle
 // electron starts --> app process is  created --> app ready events --> app closes down
+// BrowserWindow: will start new Browser Window in electron
+const { app, BrowserWindow } = electron; 
+
+    // we can also load website such as follows
+    // mainWindow.loadURL('https://www.google.com/');
+
+// any framework such as react, angular, vue can be inside this to product html,
+//  but for this section, plane html is being used
+
+// webpage can only read file which only user purposefully feeds
+// with Electron, we can read any file on OS without asking user
