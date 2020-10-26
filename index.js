1aa65f5dcb2f6c0e92e708b112f07d7c35af4c70
kapil kathuria <kapil.kathuria@gmail.com>
2020-10-26 09:00:59 +0530

    Made node integration default. Added eslint and prettier to project

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 'use scrict';
 
 const electron = require('electron');
-const path = require('path') 
+const path = require('path');
 require('dotenv').config();
 
-const env = process.env.NODE_ENV || 'development'; 
-
-// If development environment 
-if (env === 'development') { 
-	require('electron-reload')(__dirname, { 
-		electron: path.join(__dirname, 'node_modules', '.bin', 'electron'), 
-		hardResetMethod: 'exit'
-	}); 
-} 
-
+const env = process.env.NODE_ENV || 'development';
+// If development environment
+if (env === 'development') {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+    hardResetMethod: 'exit',
+  });
+}
 
 // electron starts --> app process is  created --> app ready events --> app closes down
 // BrowserWindow: will start new Browser Window in electron
-const { app, BrowserWindow } = electron; 
+const { app, BrowserWindow } = electron;
 
-app.on( 'ready',() => {
-    // do something
-    const mainWindow = new BrowserWindow ({});
-    console.log('App is now ready');
-    // we can also load website such as follows
-    // mainWindow.loadURL('https://www.google.com/');
-    // mainWindow.loadURL(`file://${__dirname}/index.html`);
-    mainWindow.loadFile('index.html');
+app.on('ready', () => {
+  // do something
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  console.log('App is now ready');
+  // we can also load website such as follows
+  // mainWindow.loadURL('https://www.google.com/');
+  // mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.loadFile('index.html');
 });
--- section2.js
+++ section2.js
@@ -14,3 +14,17 @@ const { app, BrowserWindow } = electron;
 
 // webpage can only read file which only user purposefully feeds
 // with Electron, we can read any file on OS without asking user
+
+// Our Project: user uploads a video, application to find length of video and show to user using 
+//  local application ffmpeg
+// ffmpeg is installed manually along npm package fluent-ffmpeg
+// our flow is: get file path from electron app, give it ot ffmpeg which is installed in local os, 
+//  get video length and show back to user in electron app
+
+// question for us is: should we get video length from web compoonent (index.html) or from electron app (index.js)
+//  convention in  this course: we will  doing as much as possible in electron app 
+//      web app: only web related stuff
+//      this also helps from the prospective that this web component code can now be used in actual web app as  well
+//  this required communication between index.html and index.js
+
+// IPC (Inter Process Communication): this helps with communication between index.html and index.js
